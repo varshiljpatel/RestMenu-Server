bf391c097bda1a0ecf365fbcd3c267eb31e62d39
Varshil J Patel <123259416+varshiljpatel@users.noreply.github.com>
2023-09-17 11:32:35 +0530

    Delete server directory from main

--- server/routes/api.route.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const apiController = require('../controllers/api.controller.js');
-
-router.all('/', (req, res) => apiController.getHome(req, res));
-
-module.exports = router;
